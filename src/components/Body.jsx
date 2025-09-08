8f983186178d2f8699f7171e3faa98462e5e5f1b
veerender nemali <nemaliveerender2001@gmail.com>
2025-09-08 23:25:47 +0530

    added routes

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
+
+const Body = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Body;
